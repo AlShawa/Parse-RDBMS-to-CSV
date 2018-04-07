95a8a026f35751fad3860b1fd5e2363665936a86
AlShawa <37331558+AlShawa@users.noreply.github.com>
2018-04-07 18:55:49 +0300

    Add files via upload

--- Parse.py
+++ Parse.py
@@ -1,10 +1,10 @@
 import mysql.connector
 import csv
-con=mysql.connector.connect(user='root',password='Digital5%5',host='localhost', database='ali')
+con=mysql.connector.connect(user='root',password='******',host='localhost', database='ali')
 m=con.cursor()
 q='SELECT * FROM parse'
 m.execute(q)
-with open('output.csv', 'w') as csvfile:
+with open('Parse.csv', 'w') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow([ i[0] for i in m.description ]) 
         writer.writerows(m.fetchall())
